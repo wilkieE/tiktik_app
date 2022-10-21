76eb10e645245b7b095684b3e6f3bc4d4be5c2ff
Ejiroghene Philip Wilkie <48595932+wilkieE@users.noreply.github.com>
2022-10-21 08:14:05 +0300

    Added Post Upload page

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -69,12 +69,12 @@ const VideoCard: NextPage<IProps> = ({ post }: IProps) => {
           }}
           className="rounded-3xl"
         >
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               ref={videoRef}
               loop
               src={post.video.asset.url}
-              className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
             ></video>
           </Link>
           {isHover && (
---
+++ pages/detail/[id].tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+
+const Detail = () => {
+  return <div>Detail</div>;
+};
+
+export default Detail;
