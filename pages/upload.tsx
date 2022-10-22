fe733b278bbcf8ca6cc0fad11be1129d167bbfdd
Ejiroghene Philip Wilkie <48595932+wilkieE@users.noreply.github.com>
2022-10-22 18:49:58 +0300

    Added Like button and Video page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,6 +8,7 @@ import { client } from "../utils/client";
 import { SanityAssetDocument } from "@sanity/client";
 import { topics } from "../utils/constants";
 import { type } from "os";
+import { BASE_URL } from "../utils";
 
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -62,7 +63,7 @@ const Upload = () => {
         topic: category,
       };
 
-      await axios.post("http://localhost:3000/api/post", document);
+      await axios.post(`${BASE_URL}/api/post`, document);
       router.push("/");
     }
   };
